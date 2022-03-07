69a98ef8272f448ed35d7324910ba74fb6c87aa6
Garrett <ga.landin89@gmail.com>
2022-03-07 15:55:32 -0600

    Running through commit process with JJ

--- game_board.py
+++ game_board.py
@@ -3,7 +3,7 @@ import player
 import AI
 
 def rules_of_game():
-    rules= print( " Rock crushes Scissors, /n Scissors cuts Paper, /n Paper covers Rock, /n Rock crushes Lizard, /n Lizard poisons Spock,/n Spock smashes Scissors, /n Scissors decapitates Lizard, /n Lizard eats Paper, /n Paper disproves Spock, /n Spock vaporizes Rock ")
+    rules = print( " Rock crushes Scissors, /n Scissors cuts Paper, /n Paper covers Rock, /n Rock crushes Lizard, /n Lizard poisons Spock,/n Spock smashes Scissors, /n Scissors decapitates Lizard, /n Lizard eats Paper, /n Paper disproves Spock, /n Spock vaporizes Rock ")
 
     print(rules)
 
