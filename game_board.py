f9820d3b2f75a95fc5f53822f680d0515c9147be
Garrett <ga.landin89@gmail.com>
2022-03-07 14:37:08 -0600

    Created display_greeting method in game_board.py to create practice merge conflict

--- game_board.py
+++ game_board.py
@@ -6,3 +6,6 @@ def rules_of_game():
     rules= print( " Rock crushes Scissors, /n Scissors cuts Paper, /n Paper covers Rock, /n Rock crushes Lizard, /n Lizard poisons Spock,/n Spock smashes Scissors, /n Scissors decapitates Lizard, /n Lizard eats Paper, /n Paper disproves Spock, /n Spock vaporizes Rock ")
 
     print(rules)
+
+def display_greeting():
+    print("Welcome to Rock, Paper, Scissors, Lizard, Spock")
